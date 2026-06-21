6161652fc7d2f2ff3120df8e7cdda8475514b37e
mithanizulfikar <mithanizulfikar@gmail.com>
2026-06-21 05:29:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,20 @@ document.getElementById("regForm").addEventListener("submit", function(e){
 
   const formData = new FormData(this);
   const data = {};
-  formData.forEach((val, key) => data[key] = val);
-
   fetch("https://mithanizulfikar.github.io/Learnwithzulfee/", {
-    method: "POST",
-    body: JSON.stringify(data)
-  }).then(res => {
-    alert("Registration Successful. Confirmation email sent.");
-    document.getElementById("regForm").reset();
-  }).catch(() => {
-    alert("Error submitting form");
-  });
+  method: "POST",
+  headers: {
+    "Content-Type": "text/plain"
+  },
+  body: JSON.stringify(data)
+})
+.then(res => res.text())
+.then(res => {
+  alert("Registration Successful. Email sent!");
+  document.getElementById("regForm").reset();
+})
+.catch(err => {
+  console.log(err);
+  alert("Error submitting form");
+});
 });
